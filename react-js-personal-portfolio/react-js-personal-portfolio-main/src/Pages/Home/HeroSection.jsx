bb017b8906a8f7ebe9d2fc0654120ce43661cfed
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-02-20 22:19:32 +0200

    initial commit

---
+++ react-js-personal-portfolio/react-js-personal-portfolio-main/src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,17 @@
+export default function HeroSection() {
+  return;
+  <section id="heroSection" className="hero-section">
+    <div className="hero-section-content-box">
+      <div className="hero-section-content">
+        <p className="section-title">Hey, I'm Manny </p>
+        <h1 className="hero-section-title">
+          <span className="hero-section-title-color">Front-End</span>
+          {""}
+          <br />
+          Developer
+        </h1>
+        <p className="hero-section-description"></p>
+      </div>
+    </div>
+  </section>;
+}
