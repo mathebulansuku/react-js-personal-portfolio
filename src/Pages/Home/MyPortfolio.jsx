4a61125fd998534a21d13893812a409dac07fa22
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-02-21 21:31:57 +0200

    Creating portfolio section

---
+++ src/Pages/Home/MyPortfolio.jsx
@@ -0,0 +1,12 @@
+import data from "../../data/index.json";
+
+export default function MyPortfolio() {
+  return;
+  <section className="portfolio-section" id="MyPortfolio">
+    <div className="portfolio-container-box">
+      <div className="portfolio-container">
+        <p className="sub-title">Recent Projects</p>
+      </div>
+    </div>
+  </section>;
+}
