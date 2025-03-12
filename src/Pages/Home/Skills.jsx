7232d7591ae8119c67bdac445612d7e2125e99d9
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-12 20:56:51 +0200

    Uploaded certificates

--- src/Pages/Home/Skills.jsx
+++ src/Pages/Home/Skills.jsx
@@ -1,11 +1,19 @@
-export default function Skills() {
-  return;
-  <ul>
-    <li>HTML & CSS</li>
-    <li>JavaScript</li>
-    <li>React</li>
-    <li>C++</li>
-    <li>Cloud Computing</li>
-    <li>Cloud Systems Design</li>
-  </ul>;
+export default function Accolades() {
+  return (
+    <div className="certificate-container">
+      <img
+        src="/certificates/aws-cloud-practitioner.png"
+        alt="AWS Cloud Practitioner"
+      />
+      <img src="" alt="AWS SAA" />
+      <img
+        src="/certificates/aws-solutions-architect-professional.png"
+        alt="AWS SAP"
+      />
+      <img
+        src="/certificates/aws-security-specialty.png"
+        alt="AWS Security Specialty"
+      />
+    </div>
+  );
 }
